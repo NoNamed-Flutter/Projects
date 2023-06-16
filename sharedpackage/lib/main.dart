ea1bd315513028bd2fef80f86639c61247fda279
ljyo2o9 <yggosxoljy@gmail.com>
2023-06-16 10:48:21 +0900

    shared_preferences2
    ID와 PW

--- sharedpackage/lib/main.dart
+++ sharedpackage/lib/main.dart
@@ -8,75 +8,110 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MemoPage(),
+      home: HomePage(),
     );
   }
 }
 
-class MemoPage extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  _MemoPageState createState() => _MemoPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MemoPageState extends State<MemoPage> {
-  TextEditingController _textController = TextEditingController();
+class _HomePageState extends State<HomePage> {
+  String _id = '';
+  String _pw = '';
+  SharedPreferences? _prefs; //내부 저장소에 저장되어있는 값을 불러오기 위한 변수
 
-  String _memo = ""; //메모 보여주는 곳
-
-  SharedPreferences? _prefs; //메모 저장하는 곳
+  final idController = TextEditingController(); //TextField에 값을 받아오기 위한 변수1
+  final pwController = TextEditingController(); //TextField에 값을 받아오기 위한 변수2
 
   @override
   void initState() {
+    //시작될때 한번만 load
     super.initState();
-    SharedPreferences.getInstance().then((value) {
-      //객체를 돌려쓰기?
-      setState(() {
-        _prefs = value;
-      });
-      String? memo = _prefs?.getString("memo");
-      if (memo != null) {
-        setState(() {
-          _memo = memo;
-          _textController.text = memo;
-        });
-      }
+    _loadId(); //시작하고 _loadId함수를 불러온다
+  }
+
+  _loadId() async {
+    //_loadId함수(비동기)
+    _prefs = await SharedPreferences
+        .getInstance(); // 캐시에 저장되어있는 값을 불러온다.(불러 올때까지 대기)
+    setState(() {
+      // 캐시에 저장된 값을 반영하여 현재 상태를 설정한다.
+      // SharedPreferences에 id, pw로 저장된 값을 읽어 필드에 저장. 없을 경우 0으로 대입
+      _id = (_prefs!.getString('id') ?? '');
+      _pw = (_prefs!.getString('pw') ?? '');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("메모 앱"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _textController,
-              maxLines: null,
-              decoration: InputDecoration(
+      appBar: AppBar(title: const Text('shared_preference')),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(16, 30, 16, 0),
+            child: TextField(
+              controller: idController, // 여기서 입력받은 String값을 idController에 전달
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: "메모를 입력하세요",
+                labelTex  t: "ID",
               ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  _memo = _textController.text;
-                });
-                await _prefs?.setString("memo", _memo);
-              },
-              child: Text("저장하기"),
+          ),
+          Container(
+            margin: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: pwController, // 여기서 입력받은 String값을 pwController에 전달
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "PW",
+              ),
             ),
-            SizedBox(height: 16.0),
-            Text("저장된 메모:\n$_memo"),
-          ],
-        ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(40, 20, 40, 40),
+            child: FloatingActionButton(
+              child: const Icon(Icons.account_box),
+              onPressed: () {
+                _id = idController.text; // _id 에 입력받은 값 넣어줌
+                _pw = pwController.text; // _pw 에 입력받은 값 넣어줌
+                _prefs!.setString(
+                    'id', _id); // 내부 저장소에서 id를 키로 가지고 있는 값에 입력받은 _id를 넣어줌.
+                _prefs!.setString(
+                    'pw', _pw); // 내부 저장소에서 pw를 키로 가지고 있는 값에 입력받은 _pw를 넣어줌.
+
+                showDialog(
+                  // 버튼을 눌렀음을 확인시키기 위한 창 띄우기
+                  context: context,
+                  builder: (context) {
+                    return const AlertDialog(
+                      content: Text(
+                        '저장이 완료됐습니다.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Text('아이디: $_id',
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+              )), // 어플을 재시작해도 데이터가 보존되는지 확인하기 위한 Text창
+          Text('비밀번호: $_pw',
+              style: const TextStyle(fontWeight: FontWeight.w700)),
+        ],
       ),
     );
   }
